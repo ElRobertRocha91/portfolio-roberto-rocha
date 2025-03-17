a4d9fd32c700ef364c7ab6dcddee55eddb845eff
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2025-03-16 23:44:06 -0300

    add new changes in <header> style z-10 and <Link> href=#rute, onClick in <listMenu>

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,21 +14,23 @@ export default function Navbar() {
     }
 
     return (
-        <header className="fixed top-0 left-0 w-full bg-[--background-header] backdrop-blur-lg z-5">
+        <header className="fixed top-0 left-0 w-full bg-[--background-header] backdrop-blur-lg z-10">
             <div className="flex items-center justify-between py-2 px-2 sm:px-6 lg:px-16">
                 <div className="flex-shrink-0 text-white">
                     <Link href="#" className="text-lg sm:text-4xl font-extralight ms:font-extrabold"><code>{"<"}</code>Roberto <span className="text-shadow-header">Rocha<code>{"/>"}</code></span></Link>
                 </div>
                 <nav className="flex items-center text-xl font-medium hidden lg:block">
-                    <Link href="#" className="mx-4 hover:text-[#00ffee] hover:underline hover:underline-offset-4 hover:decoration-2 
+                    <Link href="#home" className="mx-4 hover:text-[#00ffee] hover:underline hover:underline-offset-4 hover:decoration-2 
                     focus:text-[#00ffee] focus:underline focus:underline-offset-4 focus:decoration-2 active:underline active:text-[#00ffee;]">Home</Link>
-                    <Link href="#" className="mx-4 hover:text-[#00ffee] hover:underline hover:underline-offset-4 hover:decoration-2 
+                    <Link href="#about" className="mx-4 hover:text-[#00ffee] hover:underline hover:underline-offset-4 hover:decoration-2 
+                    focus:text-[#00ffee] focus:underline focus:underline-offset-4 focus:decoration-2 active:underline active:text-[#00ffee;]">About</Link>
+                    <Link href="#education" className="mx-4 hover:text-[#00ffee] hover:underline hover:underline-offset-4 hover:decoration-2 
                     focus:text-[#00ffee] focus:underline focus:underline-offset-4 focus:decoration-2 active:underline active:text-[#00ffee;]">Education</Link>
-                    <Link href="#" className="mx-4 hover:text-[#00ffee] hover:underline hover:underline-offset-4 hover:decoration-2 
+                    <Link href="#services" className="mx-4 hover:text-[#00ffee] hover:underline hover:underline-offset-4 hover:decoration-2 
                     focus:text-[#00ffee] focus:underline focus:underline-offset-4 focus:decoration-2 active:underline active:text-[#00ffee;]">Services</Link>
-                    <Link href="#" className="mx-4 hover:text-[#00ffee] hover:underline hover:underline-offset-4 hover:decoration-2 
+                    <Link href="#proyects" className="mx-4 hover:text-[#00ffee] hover:underline hover:underline-offset-4 hover:decoration-2 
                     focus:text-[#00ffee] focus:underline focus:underline-offset-4 focus:decoration-2 active:underline active:text-[#00ffee;]">Proyects</Link>
-                    <Link href="#" className="mx-4 hover:text-[#00ffee] hover:underline hover:underline-offset-4 hover:decoration-2 
+                    <Link href="#contact" className="mx-4 hover:text-[#00ffee] hover:underline hover:underline-offset-4 hover:decoration-2 
                     focus:text-[#00ffee] focus:underline focus:underline-offset-4 focus:decoration-2 active:underline active:text-[#00ffee;]">Contact</Link>
                 </nav>
                 {/* MENU BUTTON HAMBURGER */}
@@ -36,7 +38,7 @@ export default function Navbar() {
             </div>
             {/* MENU LIST RESPONSIVE */}
             {isOpen && (
-                <ListMenu/>
+                <ListMenu onClick={toggleMenu}/>
             )}
         </header>
     )
--- src/components/ui/listMenu.jsx
+++ src/components/ui/listMenu.jsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 
-export default function ListMenu() {
+export default function ListMenu({ onClick }) {
     return (
         <div className="lg:hidden">
             <div className={`px-2 pt-2 pb-3 space-y-1 sm:px-3 transform transition-all duration-700 ease-in`} >
-                <Link href="/#" className="block pt-2 text-center hover:text-[#00ffee]">Home</Link>
-                <Link href="/#" className="block pt-2 text-center hover:text-[#00ffee]">Education</Link>
-                <Link href="/#" className="block pt-2 text-center hover:text-[#00ffee]">Services</Link>
-                <Link href="/#" className="block pt-2 text-center hover:text-[#00ffee]">Proyects</Link>
-                <Link href="/#" className="block pt-2 text-center hover:text-[#00ffee]">Contact</Link>
+                <Link href="#home" onClick={onClick} className="block pt-2 text-center hover:text-[#00ffee]">Home</Link>
+                <Link href="#about" onClick={onClick} className="block pt-2 text-center hover:text-[#00ffee]">About</Link>
+                <Link href="#education" onClick={onClick} className="block pt-2 text-center hover:text-[#00ffee]">Education</Link>
+                <Link href="#services" onClick={onClick} className="block pt-2 text-center hover:text-[#00ffee]">Services</Link>
+                <Link href="#proyects" onClick={onClick} className="block pt-2 text-center hover:text-[#00ffee]">Proyects</Link>
+                <Link href="#contact" onClick={onClick} className="block pt-2 text-center hover:text-[#00ffee]">Contact</Link>
             </div>
         </div>
     )
